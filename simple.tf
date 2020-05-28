393648f3ab4d872867edae99d6db06bf5b2b745f
MarianoRubio <33099855+MarianoRubio@users.noreply.github.com>
2020-05-28 12:28:21 +0100

    Update simple.tf

--- simple.tf
+++ simple.tf
@@ -1,3 +1,11 @@
 data "template_file" "example" {
   template = "${var.example}"
 }
+resource "random_id" "server" {
+  keepers = {
+    # Generate a new id each time we switch to a new AMI id
+    ami_id = "${var.ami_id}"
+  }
+
+  byte_length = 8
+}
