459dc718614f26e5307fe9a42500c574fb0aeabe
MarianoRubio <33099855+MarianoRubio@users.noreply.github.com>
2020-05-29 11:19:03 +0100

    Update simple.tf

--- simple.tf
+++ simple.tf
@@ -3,7 +3,7 @@ data "template_file" "example" {
 }
 resource "random_id" "server" {
   keepers = {
-    # Generate a new id each time we switch to a new AMI id
+    # Generate a new id each time we switch to a new AMI id test update
     ami_id = "${var.ami_id}"
   }
 
